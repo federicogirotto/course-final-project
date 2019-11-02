eacd13d60d66dd123d15286f9450e3a3d7d9d087
Federico Girotto <federicogirotto@MacBook-Pro-di-Federico-4.local>
2019-11-02 19:44:28 +0100

    website_official

--- src/about/main.js
+++ src/about/main.js
@@ -1,1 +1,25 @@
 import "../styles/main.styl";
+
+// Page ABOUT
+
+
+const controller3 = new ScrollMagic.Controller();
+
+const portfolio = document.querySelector(".container-team");
+console.log(portfolio.clientWidth);
+const slideAnimation = new TimelineMax()
+.fromTo(".container-team", 1,{ x: "0vw", y: "20vh" }, { x: "-100vw", y: "20vh",ease: Power0.easeNone  },  0)
+.fromTo(".container-arrows", 2,{ x: "100vw", y: "20vh" }, { x: "-100vw", y: "20vh",ease: Power0.easeNone  },  0)
+.fromTo(".team-ph-box", 2,{ x: "100vw", y: "-35vh" }, { x: "-100vw", y: "-35vh",ease: Power0.easeNone  },  1)
+.fromTo(".contact", 2,{ x: "100vw", y: "-80vh" }, { x: "-100vw", y: "-80vh",ease: Power0.easeNone  },  2)
+
+// create scene to pin and link animation
+new ScrollMagic.Scene({
+  triggerElement: ".container-all",
+  triggerHook: "onLeave",
+  duration: "400%"
+})
+  .setPin(".container-all")
+  .setTween(slideAnimation)
+
+  .addTo(controller3);
--- src/projects/main.js
+++ src/projects/main.js
@@ -9,9 +9,9 @@ const controller2 = new ScrollMagic.Controller();
 const portfolio = document.querySelector(".project-box");
 console.log(portfolio.clientWidth);
 const slideAnimation = new TimelineMax()
-.fromTo(".box-1", 1,{ x: "10vw", y: "35vh" }, { x: "-100vw", y: "35vh",  },  0)
-.fromTo(".box-2", 1.4,{ x: "50vw", y: "35vh" }, { x: "-100vw", y: "35vh",  },  0)
-.fromTo(".box-3", 1.8,{ x: "90vw", y: "35vh" }, { x: "-100vw", y: "35vh",  },  0)
+.fromTo(".box-1", 1,{ x: "10vw", y: "35vh" }, { x: "-100vw", y: "35vh", },  0)
+.fromTo(".box-2", 1.4,{ x: "50vw", y: "35vh" }, { x: "-100vw", y: "35vh", },  0)
+.fromTo(".box-3", 1.8,{ x: "90vw", y: "35vh" }, { x: "-100vw", y: "35vh", },  0)
 .fromTo(".box-4", 2.2,{ x: "130vw", y: "35vh" }, { x: "-100vw", y: "35vh",  },  0)
 .fromTo(".box-5", 2.6,{ x: "170vw", y: "35vh" }, { x: "-100vw", y: "35vh",  },  0)
 
